f5e6ced2684548530c10ca3494306b8a8da018d6
NKpy07 <128097264+NKpy07@users.noreply.github.com>
2024-09-18 12:28:21 +0200

    .exe

--- zrun/bala.py
+++
@@ -1,8 +0,0 @@
-from entidad import Entidad
-class Bala(Entidad):
-    def __init__(self, x, y):
-        super().__init__(x, y, 10, 20, 40, "img/bala.png")
-    
-    def movimiento(self):
-        self.y -= self.velocidad
-        self.rect.topleft = (self.x, self.y)
--- zrun/personaje.py
+++
@@ -1,4 +0,0 @@
-from entidad import Entidad
-class Cubo(Entidad):
-    def __init__(self, x, y):
-        super().__init__(x, y, 60, 60, 10, "img/personaje.png")
